d6ebc2fe75df0d660a5da3d3d521c21dd7173483
Josh Taylor <josh@zerrtech.com>
2018-06-28 14:39:47 -0600

    Mocking PUT/PATCH requests and removed POST (DELETE Not supported either)

--- starwars/models/hero/view.py
+++ starwars/models/hero/view.py
@@ -2,12 +2,17 @@ from rest_framework import viewsets, mixins
 from starwars.models.hero.serializer import HeroSerializer
 from starwars.models.hero.hero import Hero
 from starwars.authentication.Auth0 import Auth0Authentication
+from rest_framework.response import Response
+import json
 
-class HeroViewSet(mixins.CreateModelMixin,
-                  mixins.ListModelMixin,
+class HeroViewSet(mixins.ListModelMixin,
                   mixins.UpdateModelMixin,
                   viewsets.GenericViewSet):
 
     authentication_classes = (Auth0Authentication,)
     queryset = Hero.objects.all()
     serializer_class = HeroSerializer
+
+    def update(self, request, *args, **kwargs):
+        # Mock the update so we don't actually modify the database
+        return Response(json.loads(request.body))
